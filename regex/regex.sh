51f3b2079d408affe85caa175d0f935bd4444c58
Johannes Martinez <mart4695@morris.umn.edu>
2020-09-06 23:03:39 -0500

    Use awk to find patterns in input 1
    Use awk and regex groupings to select patterns in input text.
    Pipe the output of awk to cat, and write text to output file.

---
+++ regex/regex.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Pipe the output of the sed regex "filter" (which takes as an input r0_input.txt) to a newly
+# Created output file using cat
+sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt | cat > r0_output.txt
+
+# Use awk and regex groupings to select patterns in input text.
+# Pipe the output of awk to cat, and write text to output file. 
+awk 'match($0, /([a-zA-Z]+)\. ([a-zA-Z ]+) ([a-zA-Z]+)\./, groups) {print "1. " groups[1] "\n" "2. " groups[3] "\n" }' < r1_input.txt |
+cat > r1_output.txt
